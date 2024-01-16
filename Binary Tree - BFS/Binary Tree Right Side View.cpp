d353f1b4ff5265289652cbfd31b6c17119da87da
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2024-01-16 18:17:53 +0530

    Binary Tree Right Side View.cpp

---
+++ Binary Tree - BFS/Binary Tree Right Side View.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        TreeNode* current = root;
+        vector<int> value;
+
+        if (root == NULL) {
+            return value;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                // If it's the last node in the level, add its value to the
+                // result
+                if (i == levelSize - 1) {
+                    value.push_back(temp->val);
+                }
+                if (temp->left != nullptr) {
+                    q.push(temp->left);
+                }
+                if (temp->right != nullptr) {
+                    q.push(temp->right);
+                }
+            }
+        }
+
+        return value;
+    }
+};
+
+// Example 1:
+
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+
+// Example 2:
+
+// Input: root = [1,null,3]
+// Output: [1,3]
+
+// Example 3:
+
+// Input: root = []
+// Output: []
