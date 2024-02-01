b35308cdaee439a1ed84706be57a2f31b4d90e10
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2024-02-01 22:29:16 +0530

    Find Peak Element.cpp

---
+++ Binary Search/Find Peak Element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int high = nums.size()-1;
+        while(low < high){
+            int mid = (low + high) >> 1;
+            if(nums[mid] > nums[mid+1]){
+                high = mid;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: 2
+// Explanation: 3 is a peak element and your function should return the index number 2.
+
+// Example 2:
+
+// Input: nums = [1,2,1,3,5,6,4]
+// Output: 5
+// Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
