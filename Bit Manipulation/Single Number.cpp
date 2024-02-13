a31a4a57b8e354c4683f663e951cb9710596b887
Tanaya Naik <78205020+NaikTanaya@users.noreply.github.com>
2024-02-13 21:49:12 +0530

    Single Number.cpp

---
+++ Bit Manipulation/Single Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
+    }
+};
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+// Example 3:
+
+// Input: nums = [1]
+// Output: 1
+ 
